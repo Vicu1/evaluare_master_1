59ebc63d73a4833e9b97b512638f3f311d92dafc
vicug <vicugaideic@gmail.com>
2025-05-14 23:43:57 +0300

    feature-a

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 def main():
-    print("Hello, CI/CD! Aplicația funcționează corect.")
+    print("Hello, CI/CD! Aplicația funcționează corect branch feature-a.")
 
 if __name__ == "__main__":
     main()
